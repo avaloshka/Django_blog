2169173fde755981fda08ebeebc095da93d213a9
Alex Valoshka <alexvaloshka@gmail.com>
2022-12-31 13:09:37 +0000

    added requirements.txt

--- application/views.py
+++ application/views.py
@@ -21,7 +21,7 @@ def post_detail(request, slug):
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
 
-            # Create Comment object but don't save to database yet
+            # Create Comment object but don't post it yet
             new_comment = comment_form.save(commit=False)
             # Assign the current post to the comment
             new_comment.post = post
